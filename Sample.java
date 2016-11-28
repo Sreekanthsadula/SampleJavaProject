e11f76686b0e2be998b617377b290168e9654f27
Sreekanthsadula <sreekanth.sadula@dot.state.fl.us>
2016-11-28 12:23:02 -0500

    Adding code for Listing the directory

--- Sample.java
+++ Sample.java
@@ -1,1 +1,18 @@
 
+Iterable<Path> dirs = FileSystems.getDefault().getRootDirectories();
+for (Path name: dirs) {
+    System.err.println(name);
+}
+
+
+Path dir = ...;
+try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+    for (Path file: stream) {
+        System.out.println(file.getFileName());
+    }
+} catch (IOException | DirectoryIteratorException x) {
+    // IOException can never be thrown by the iteration.
+    // In this snippet, it can only be thrown by newDirectoryStream.
+    System.err.println(x);
+}
+  
